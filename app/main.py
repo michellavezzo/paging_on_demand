08e7f59786a636c2c61647171c9a27b96972cd5d
Michel Lavezzo <michellavezzo@gmail.com>
2023-09-20 17:21:00 -0300

    feat: reading file

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
-# Open the file for reading
-with open('input.txt', 'r') as f:
-    # Initialize an empty list to store the number pairs
-    number_pairs = []
+def read_number_pairs(filename):
+    """
+    Read pairs of numbers from a file.
     
-    # Loop through each line in the file
-    for line in f:
-        # Split the line by spaces to get the two numbers
-        num1, num2 = map(int, line.split())
-        
-        # Append the numbers as a tuple to the list
-        number_pairs.append((num1, num2))
+    Args:
+    - filename (str): The path to the file to read.
+    
+    Returns:
+    - list of tuple: A list of tuples where each tuple contains a pair of numbers.
+    """
+    number_pairs = []
 
-# Print the pairs
-for pair in number_pairs:
-    print(pair[0], pair[1])
+    # Open the file for reading
+    with open(filename, 'r') as f:
+        # Loop through each line in the file
+        for line in f:
+            # Split the line by spaces to get the two numbers
+            num1, num2 = map(int, line.split())
+            
+            # Append the numbers as a tuple to the list
+            number_pairs.append((num1, num2))
+    
+    return number_pairs
+
+# Example usage
+pairs = read_number_pairs('input.txt')
+print(pairs)
